abdd187190f2adb144c55009c9636e0655f12e1f
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-08-25 10:38:32 +0900

    Add -no-memory-leak.cpp

---
+++ src/no-memory-leak.cpp
@@ -0,0 +1,20 @@
+// g++ -o memory-leak memory-leak.cpp -fsanitize=address -fno-omit-frame-pointer -g
+// clang++ -o memory-leak memory-leak.cpp -fsanitize=address -fno-omit-frame-pointer -g
+
+#include <iostream>
+int main() {
+#if defined(__clang__)
+#if __has_feature(address_sanitizer) //clang
+    std::cout << "Has address_sanitiezer (clang)\n";
+#endif
+#else
+#if defined(__SANITIZE_ADDRESS__) // gcc
+    std::cout << "Has address_sanitiezer (gcc)\n";
+#endif
+#endif
+    std::cout << "Started Program\n";
+    int* i = new int(); 
+    *i = 42;
+    std::cout << "Expected i: " << *i << std::endl;
+    delete i;
+}
