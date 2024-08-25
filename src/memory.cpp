f52f2065dcdfd1ec4bc04e5c27d80775a2ecac49
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-08-25 01:06:25 +0000

    Add has_asan()

--- src/memory.cpp
+++ src/memory.cpp
@@ -15,3 +15,16 @@ void memory_leak() {
     *i = 42;
     std::cout << "Expected i: " << *i << std::endl;
 }
+
+// [[Rcpp::export]]
+void has_asan() {
+#if defined(__clang__)
+#if __has_feature(address_sanitizer) //clang
+    std::cout << "Has address_sanitiezer (clang)\n";
+#endif
+#else
+#if defined(__SANITIZE_ADDRESS__) // gcc
+    std::cout << "Has address_sanitiezer (gcc)\n";
+#endif
+#endif
+}
